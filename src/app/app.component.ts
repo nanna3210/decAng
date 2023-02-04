afe19b3189d3eefaaefbf26b7d89167063f8ce6b
nanna <navkishoredas@gmail.com>
2023-02-04 22:32:01 +0530

    initialize

---
+++ src/app/app.component.ts
@@ -0,0 +1,28 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss']
+})
+export class AppComponent {
+  title = 'decAng';
+  nanna ="hey NNAnannan"; 
+// function to get cricket score?
+
+
+                                                                                           
+
+
+
+
+
+
+
+
+
+
+
+
+   
+}
