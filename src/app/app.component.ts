ef5fd981bb1a88fced889e52ddf0da074d56f3fc
nanna <navkishoredas@gmail.com>
2023-02-05 23:10:56 +0530

    lets intialize try

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,28 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
   title = 'decAng';
-  nanna ="hey NNAnannan"; 
-// function to get cricket score?
+  nanna = 'hey NNAnannan';
+  // function to get cricket score?
 
+  constructor(@Inject(DOCUMENT) private document: Document) {}
 
-                                                                                           
-
-
-
-
-
-
-
-
-
-
-
-
-   
+  ngOnInit(): void {
+    console.log(this.document);
+  }
 }
