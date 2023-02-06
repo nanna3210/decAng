9c159282326dfe0f4f430ebbb431184e53060847
nanna <navkishoredas@gmail.com>
2023-02-06 17:54:01 +0530

    com msg /3 if demo component

--- src/app/ifdemo/ifdemo.component.ts
+++ src/app/ifdemo/ifdemo.component.ts
@@ -3,13 +3,16 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-ifdemo',
   templateUrl: './ifdemo.component.html',
-  styleUrls: ['./ifdemo.component.scss']
+  styleUrls: ['./ifdemo.component.scss'],
 })
 export class IfdemoComponent implements OnInit {
-
-  constructor() { }
+  users: any = {};
+  constructor() {}
 
   ngOnInit(): void {
+    fetch('https://randomuser.me/api/')
+      // .then((response) => response.json)
+      .then((data) => console.log(data))
+      .then((data) => (this.users = data));
   }
-
 }
