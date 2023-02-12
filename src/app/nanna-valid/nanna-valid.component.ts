189b3fb3aa1df96e20f31cb5b04753a59ea22006
nanna <navkishoredas@gmail.com>
2023-02-12 06:05:03 +0530

    change valid

--- src/app/nanna-valid/nanna-valid.component.ts
+++ src/app/nanna-valid/nanna-valid.component.ts
@@ -1,27 +1,48 @@
 import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup, FormGroupName } from '@angular/forms';
+import {
+  FormBuilder,
+  FormControl,
+  FormGroup,
+  FormGroupName,
+  Validators,
+} from '@angular/forms';
 @Component({
   selector: 'app-nanna-valid',
   templateUrl: './nanna-valid.component.html',
   styleUrls: ['./nanna-valid.component.scss'],
 })
 export class NannaValidComponent implements OnInit {
-  constructor() {}
-  frmRegister = new FormGroup({
-    Name: new FormControl(''),
-    price: new FormControl(0),
-    frmStock: new FormGroup({
-      status: new FormControl(false),
-      city: new FormControl(''),
-    }),
-    // shippedTo: new FormControl(''),
-  });
+  frmRegister: any;
+  constructor(private fb: FormBuilder) {}
+  ngOnInit(): void {
+    this.frmRegister = this.fb.group({
+      Name: this.fb.control('', [Validators.required, Validators.minLength(4)]),
+      price: this.fb.control(''),
+      frmStock: this.fb.group({
+        status: this.fb.control(false),
+        city: this.fb.control(''),
+      }),
+    });
+  }
+
+  get Name() {
+    return this.frmRegister.get('Name') as FormControl;
+  }
+
+  // frmRegister = new FormGroup({
+  //   Name: new FormControl(''),
+  //   price: new FormControl(0),
+  //   frmStock: new FormGroup({
+  //     status: new FormControl(false),
+  //     city: new FormControl(''),
+  //   }),
+  // shippedTo: new FormControl(''),
+  // });
 
   formSubmit(e: any) {
     alert(JSON.stringify(e));
   }
 
-  ngOnInit(): void {}
   // formcontrol
   // frmgroup
 }
